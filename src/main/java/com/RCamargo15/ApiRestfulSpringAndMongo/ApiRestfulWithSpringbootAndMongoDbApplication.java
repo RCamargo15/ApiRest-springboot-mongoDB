1e9b45dec050363ec083602584f5c0d6d5bb536d
RCamargo15 <rafael.camargo384@gmail.com>
2022-02-14 15:14:27 -0300

    Project created. User and UserResource endpoint.

---
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/ApiRestfulWithSpringbootAndMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.RCamargo15.ApiRestfulSpringAndMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulWithSpringbootAndMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulWithSpringbootAndMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RCamargo15/ApiRestfulWithSpringbootAndMongoDB/ApiRestfulWithSpringbootAndMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RCamargo15.ApiRestfulWithSpringbootAndMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulWithSpringbootAndMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
