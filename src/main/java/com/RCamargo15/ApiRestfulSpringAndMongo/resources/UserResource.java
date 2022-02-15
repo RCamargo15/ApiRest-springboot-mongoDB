b516705100e714dca1645fa07db9c8528f91a760
RCamargo15 <rafael.camargo384@gmail.com>
2022-02-15 09:32:41 -0300

    Obtendo usuário por ID e tratamento de exceção

--- src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/resources/UserResource.java
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import com.RCamargo15.ApiRestfulSpringAndMongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResouce {
+public class UserResource {
 
 	@Autowired
 	private UserService service;
@@ -26,5 +27,11 @@ public class UserResouce {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(o -> new UserDTO(o)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/services/UserService.java
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.RCamargo15.ApiRestfulSpringAndMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.RCamargo15.ApiRestfulSpringAndMongo.domain.User;
 import com.RCamargo15.ApiRestfulSpringAndMongo.repositories.UserRepository;
+import com.RCamargo15.ApiRestfulSpringAndMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
