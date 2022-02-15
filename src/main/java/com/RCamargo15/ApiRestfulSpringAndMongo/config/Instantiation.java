e1841098f52bf2d157994d45fd7eb543f9d15ab4
RCamargo15 <rafael.camargo384@gmail.com>
2022-02-15 16:09:51 -0300

    Entity post com user

--- src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/config/Instantiation.java
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.RCamargo15.ApiRestfulSpringAndMongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.RCamargo15.ApiRestfulSpringAndMongo.domain.Post;
 import com.RCamargo15.ApiRestfulSpringAndMongo.domain.User;
+import com.RCamargo15.ApiRestfulSpringAndMongo.repositories.PostRepository;
 import com.RCamargo15.ApiRestfulSpringAndMongo.repositories.UserRepository;
 
 @Configuration
@@ -15,16 +19,25 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(p1,p2));
 	}
 
 }
---
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.RCamargo15.ApiRestfulSpringAndMongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.RCamargo15.ApiRestfulSpringAndMongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
