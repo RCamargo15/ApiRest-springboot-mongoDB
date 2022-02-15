b23cf2ce3af5892b1b2d9afee8a0a72b44866fc5
RCamargo15 <rafael.camargo384@gmail.com>
2022-02-15 16:34:04 -0300

    PostService, PostResource, endpoint obtendo post por id

---
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.RCamargo15.ApiRestfulSpringAndMongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.RCamargo15.ApiRestfulSpringAndMongo.domain.Post;
+import com.RCamargo15.ApiRestfulSpringAndMongo.repositories.PostRepository;
+import com.RCamargo15.ApiRestfulSpringAndMongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+}
