8650693776aae66d0a5006ce1e83b26622de5cb1
RCamargo15 <rafael.camargo384@gmail.com>
2022-02-15 16:15:33 -0300

    Projeção dos dados do autor com AuthorDTO

--- src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/config/Instantiation.java
+++ src/main/java/com/RCamargo15/ApiRestfulSpringAndMongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.RCamargo15.ApiRestfulSpringAndMongo.domain.Post;
 import com.RCamargo15.ApiRestfulSpringAndMongo.domain.User;
+import com.RCamargo15.ApiRestfulSpringAndMongo.dto.AuthorDTO;
 import com.RCamargo15.ApiRestfulSpringAndMongo.repositories.PostRepository;
 import com.RCamargo15.ApiRestfulSpringAndMongo.repositories.UserRepository;
 
@@ -33,10 +34,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(p1,p2));
 	}
 
